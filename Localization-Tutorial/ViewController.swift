77a11401ce7475eeaab1106e2bc0e5bec0444370
leoiphonedev <aggarwalaman1985@gmail.com>
2018-08-17 22:41:36 +0530

    Initial Commit

---
+++ Localization-Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Localization-Tutorial
+//
+//  Created by Aman Aggarwal on 17/08/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
